fad1ff96521d349d67a20486a33ee4636e050bbf
Anto <antonia.carmazan@gmail.com>
2024-05-31 17:15:18 +0300

    commit

--- App.tsx
+++ App.tsx
@@ -1,117 +1,156 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Button, TextInput, Text, View, StyleSheet} from 'react-native';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
+import {Picker} from '@react-native-picker/picker';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const validationSchema = Yup.object().shape({
+  accountType: Yup.string()
+    .oneOf(['Advanced', 'Manual'], 'Invalid Account Type')
+    .required('Required'),
+  username: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().required('Required'),
+  serverAddress: Yup.string().required('Required'),
+  serverPath: Yup.string().when('accountType', (accountType, schema) => {
+    return accountType === 'Advanced' ? schema.required('Required') : schema;
+  }),
+  port: Yup.number().when('accountType', (accountType, schema) => {
+    return accountType === 'Advanced'
+      ? schema
+          .min(1, 'Port number must be between 1 and 65535')
+          .max(65535, 'Port number must be between 1 and 65535')
+          .required('Required')
+      : schema;
+  }),
+});
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const App = () => {
+  const formik = useFormik({
+    initialValues: {
+      accountType: '',
+      username: '',
+      password: '',
+      serverAddress: '',
+      serverPath: '',
+      port: '',
+      useSSL: false,
+    },
+    validationSchema,
+    onSubmit: values => {
+      console.log(values);
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
+    <View style={styles.container}>
+      <Text style={styles.label}>Account Type:</Text>
+      <Picker
+        selectedValue={formik.values.accountType}
+        onValueChange={itemValue =>
+          formik.setFieldValue('accountType', itemValue)
+        }>
+        <Picker.Item label="Select Account Type" value="" />
+        <Picker.Item label="Advanced" value="Advanced" />
+        <Picker.Item label="Manual" value="Manual" />
+      </Picker>
+      {formik.errors.accountType && (
+        <Text style={styles.errorText}>{formik.errors.accountType}</Text>
+      )}
+      <Text style={styles.label}>User Name:</Text>
+      <TextInput
+        style={[styles.input, formik.errors.username && {borderColor: 'red'}]}
+        placeholder="name@example.com"
+        onChangeText={formik.handleChange('username')}
+        value={formik.values.username}
+        secureTextEntry={false}
+      />
+      {formik.errors.username && (
+        <Text style={styles.errorText}>{formik.errors.username}</Text>
+      )}
+      <Text style={styles.label}>Password:</Text>
+      <TextInput
+        style={[styles.input, formik.errors.password && {borderColor: 'red'}]}
+        placeholder="Required"
+        onChangeText={formik.handleChange('password')}
+        value={formik.values.password}
+        secureTextEntry
+      />
+      {formik.errors.password && (
+        <Text style={styles.errorText}>{formik.errors.password}</Text>
+      )}
+      <Text style={styles.label}>Server Address:</Text>
+      <TextInput
         style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+          styles.input,
+          formik.errors.serverAddress && {borderColor: 'red'},
+        ]}
+        placeholder="example.com"
+        onChangeText={formik.handleChange('serverAddress')}
+        value={formik.values.serverAddress}
+      />
+      {formik.errors.serverAddress && (
+        <Text style={styles.errorText}>{formik.errors.serverAddress}</Text>
+      )}
+      {formik.values.accountType === 'Advanced' && (
+        <>
+          <Text style={styles.label}>Server Path:</Text>
+          <TextInput
+            style={[
+              styles.input,
+              formik.errors.serverPath && {borderColor: 'red'},
+            ]}
+            placeholder="/calendars/users"
+            onChangeText={formik.handleChange('serverPath')}
+            value={formik.values.serverPath}
+          />
+          {formik.errors.serverPath && (
+            <Text style={styles.errorText}>{formik.errors.serverPath}</Text>
+          )}
+          <Text style={styles.label}>Port:</Text>
+          <TextInput
+            style={[styles.input, formik.errors.port && {borderColor: 'red'}]}
+            placeholder=""
+            onChangeText={value =>
+              formik.setFieldValue('port', parseInt(value))
+            }
+            value={formik.values.port.toString()}
+            keyboardType="numeric"
+          />
+          {formik.errors.port && (
+            <Text style={styles.errorText}>{formik.errors.port}</Text>
+          )}
+        </>
+      )}
+      <Button
+        title={formik.values.useSSL ? 'Use SSL: ON' : 'Use SSL: OFF'}
+        onPress={() => formik.setFieldValue('useSSL', !formik.values.useSSL)}
+      />
+      <Button title="Submit" onPress={formik.handleSubmit} />
     </View>
   );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+    marginBottom: 10,
+    padding: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  label: {
+    fontWeight: 'bold',
+    marginBottom: 5,
   },
-  highlight: {
-    fontWeight: '700',
+  errorText: {
+    color: 'red',
   },
 });
 
